660966ee26a491d7923ce8cabd4d607e2c3c4ff4
minoring <minoh1350@gmail.com>
2021-06-25 01:58:09 +0000

    Fix detecting white ball to check all three colors

--- Project2/catkin_ws/src/ball_chaser/src/process_image.cpp
+++ Project2/catkin_ws/src/ball_chaser/src/process_image.cpp
@@ -20,8 +20,9 @@ void process_image_callback(const sensor_msgs::Image img) {
   float ang_z = 0;
 
   // Loop through each pixel in the image and determine the presence of the ball
-  for (int i = 0; i < img.height * img.step; ++i) {
-    if (img.data[i] == white_pixel) {
+  for (int i = 0; i < img.height * img.step; i += 3) {
+    if (img.data[i] == white_pixel && img.data[i + 1] == white_pixel &&
+        img.data[i + 2] == white_pixel) {
       if (i % img.step < img.step / 3) {
         ROS_INFO_STREAM("The ball is in left side");
         ang_z = 0.2;
